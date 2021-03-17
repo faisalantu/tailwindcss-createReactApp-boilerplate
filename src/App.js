a3d92e3169a1acd657ab2c18e726f7d23e5b9558
faisalantu <faisalantu@gmail.com>
2021-03-17 10:29:05 +0600

    :bug: fixed some bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import './assets/main.css'
+import React from "react";
+import "./App.css";
+import "./assets/main.css";
 
 function App() {
   return (
     <div className="App">
-      <p className="text-lg text-center mt-56 text-teal-400 font-bold">Hello Neko</p>
+      <p className="text-lg text-center mt-56 text-teal-400 font-bold">
+        Hello Neko
+      </p>
     </div>
   );
 }
---
+++ src/gg.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function gg() {
+  return <div>
+
+  </div>;
+}
+
+export default gg;
